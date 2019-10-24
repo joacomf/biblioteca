7a0228609f5bfa0a1d4e25339f846df29002e2eb
Rodrigo Mungo <rodrigo.mungo@mercadolibre.com>
2019-10-24 14:16:22 -0300

    Obtener libro por ID, implementacion de metodo y test.

--- src/main/java/ar/com/grupoesfera/biblioteca/repo/BaseDeLibros.java
+++ src/main/java/ar/com/grupoesfera/biblioteca/repo/BaseDeLibros.java
@@ -12,4 +12,16 @@ public class BaseDeLibros {
         
         return App.instancia().obtenerEntityManager().createQuery("select l from Libro l").getResultList();
     }
+
+    public Libro obtenerLibroPorId(long id) throws Exception {
+        List<Libro> libros = App.instancia()
+                .obtenerEntityManager()
+                .createQuery(String.format("select l from Libro l where id = %d", id))
+                .getResultList();
+        if (libros.size() > 0) {
+            return libros.get(0);
+        } else {
+            throw new Exception("Libro no encontrado");
+        }
+    }
 }
